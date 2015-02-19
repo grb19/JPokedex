6229b020e7ef293d7df234347d06db29534ee6c7
Steffen Seckler <seckler@in.tum.de>
2015-02-20 00:19:23 +0100

    icons

--- JPokedex/src/jpokedex/JPokedex.java
+++ JPokedex/src/jpokedex/JPokedex.java
@@ -13,11 +13,15 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
- *
+ * JPokedex is the main app of the JPokedex project.
  * @author grb19
  */
 public class JPokedex extends Application {
-    
+    /**
+     * start gets called, when application is launched
+     * @param stage the primarystage of the application
+     * @throws Exception 
+     */
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
@@ -25,14 +29,34 @@ public class JPokedex extends Application {
         stage.setTitle("JPokedex");
         stage.setScene(scene);
         stage.getIcons().add(new Image(JPokedex.class.getResourceAsStream("images/icon.png")));
-        
+        //checkForShortcut();
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+   /**
+    * Main method, that starts the program
+    * @param args 
+    */
     public static void main(String[] args) {
         launch(args);
     }
+
+    /*private void checkForShortcut() {
+        IntegrationService is;
+        try{
+            is=(IntegrationService) ServiceManager.lookup("javax.jnlp.IntegrationService");
+        }catch(UnavailableServiceException e){
+            return;
+        }
+        if(!is.hasDesktopShortcut()){
+            if(!is.requestShortcut(true, true, "JPokedex")){
+                System.out.println("shortcuts not created sucessfully");
+            }
+            else{
+                System.out.println("shortcuts created sucessfully");
+            }
+        }else{
+            System.out.println("already existent");
+        }
+    }*/
 }
