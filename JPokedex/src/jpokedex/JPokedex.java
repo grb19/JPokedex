56b0760532f0d955b3991b514c5872b7b6809e88
Steffen Seckler <seckler@in.tum.de>
2015-02-19 15:13:07 +0100

    added icon

--- JPokedex/src/jpokedex/JPokedex.java
+++ JPokedex/src/jpokedex/JPokedex.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,8 +22,10 @@ public class JPokedex extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
-        
+        stage.setTitle("JPokedex");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(JPokedex.class.getResourceAsStream("images/icon.png")));
+        
         stage.show();
     }
 
