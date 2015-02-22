d2b28896dc0d188cbff341efec3518bb805287f9
Steffen Seckler <seckler@in.tum.de>
2015-02-22 13:51:33 +0100

    First Layout of database.pokemon and pokedexes

---
+++ JPokedex/src/jpokedex/database/userdatabase/pokemon/UserPokemon.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpokedex.database.userdatabase.pokemon;
+
+/**
+ *
+ * @author grb19
+ */
+public class UserPokemon{
+    
+}
